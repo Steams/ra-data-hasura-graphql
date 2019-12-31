ea846e8ca051ffac8525eae33f890138b6cc27b3
Boris Kotov <bk@webdeb.de>
2019-12-31 02:36:06 +0100

    Add Prettier, remove unused deps

--- src/index.js
+++ src/index.js
@@ -1,29 +1,35 @@
-import { GET_ONE, GET_LIST, GET_MANY, GET_MANY_REFERENCE, DELETE, CREATE,UPDATE,UPDATE_MANY, DELETE_MANY} from './fetchActions';
-
 import merge from 'lodash/merge';
 import buildDataProvider from 'ra-data-graphql';
-
-import defaultBuildQuery from './buildQuery';
+import {
+    GET_ONE,
+    GET_LIST,
+    GET_MANY,
+    GET_MANY_REFERENCE,
+    DELETE,
+    CREATE,
+    UPDATE,
+    UPDATE_MANY,
+    DELETE_MANY,
+} from './fetchActions';
+import buildQuery from './buildQuery';
+export { buildQuery };
 
 const defaultOptions = {
-    buildQuery: defaultBuildQuery,
+    buildQuery,
     introspection: {
         operationNames: {
-            [GET_LIST]: resource => `${(resource.name)}`,
+            [GET_LIST]: resource => `${resource.name}`,
             [GET_ONE]: resource => `${resource.name}`,
-            [GET_MANY]: resource => `${(resource.name)}`,
-            [GET_MANY_REFERENCE]: resource => `${(resource.name)}`,
+            [GET_MANY]: resource => `${resource.name}`,
+            [GET_MANY_REFERENCE]: resource => `${resource.name}`,
             [CREATE]: resource => `insert_${resource.name}`,
             [UPDATE]: resource => `update_${resource.name}`,
             [UPDATE_MANY]: resource => `update_${resource.name}`,
             [DELETE]: resource => `delete_${resource.name}`,
             [DELETE_MANY]: resource => `delete_${resource.name}`,
         },
-    }
+    },
 };
-
-
-export const buildQuery = defaultBuildQuery;
 
 export default options => {
     return buildDataProvider(merge({}, defaultOptions, options)).then(
