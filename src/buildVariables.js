5470bec44f0bd4978ca134415541014b5b7aa587
steams <radclifferobs@gmail.com>
2020-01-03 22:12:56 -0500

    Add fuzzy filtering in list queries

--- src/buildVariables.js
+++ src/buildVariables.js
@@ -30,72 +30,11 @@ const buildGetListVariables = introspectionResults => (
                 ];
             }
 
-            // if filter property is an object
-            // TODO find out how to filter on Object in hasura or if this is even necessary
-            // if (typeof params.filter[key] === 'object') {
-
-            //     // find a resourceFilter type in the schema (NOTE this wont work for hasura)
-            //     const type = introspectionResults.types.find(
-            //         t => t.name === `${resource.type.name}Filter`
-            //     );
-
-            //     // find inputfields on the filter type found that match key_some ?
-            //     const filterSome = type.inputFields.find(
-            //         t => t.name === `${key}_some`
-            //     );
-
-            //     if (filterSome) {
-            //         const filter = Object.keys(params.filter[key]).reduce(
-            //             (filter_acc, k) => ({
-            //                 ...filter_acc,
-            //                 [`${k}_in`]: params.filter[key][k],
-            //             }),
-            //             {}
-            //         );
-            //         return { ...acc, [`${key}_some`]: filter };
-            //     }
-            // }
-
-            // Split a nested key, so address.street = address,street
-            // const parts = key.split('.');
-
-            // // if the filter property is deep, ie address.street...
-            // if (parts.length > 1) {
-            //     // if the second part of the filter is id, adress.id
-            //     if (parts[1] === 'id') {
-            //         const type = introspectionResults.types.find(
-            //             t => t.name === `${resource.type.name}Filter`
-            //         );
-            //         const filterSome = type.inputFields.find(
-            //             t => t.name === `${parts[0]}_some`
-            //         );
-
-            //         if (filterSome) {
-            //             return {
-            //                 ...acc,
-            //                 [`${parts[0]}_some`]: { id: params.filter[key] },
-            //             };
-            //         }
-
-            //         return { ...acc, [parts[0]]: { id: params.filter[key] } };
-            //     }
-
-            //     const resourceField = resource.type.fields.find(
-            //         f => f.name === parts[0]
-            //     );
-            //     if (resourceField.type.name === 'Int') {
-            //         return { ...acc, [key]: parseInt(params.filter[key], 10) };
-            //     }
-            //     if (resourceField.type.name === 'Float') {
-            //         return { ...acc, [key]: parseFloat(params.filter[key], 10) };
-            //     }
-            // }
-
             return [
                 ...acc,
                 {
                     [key]: {
-                        _eq: params.filter[key],
+                        _ilike: "%" + params.filter[key] + "%",
                     },
                 },
             ];
@@ -133,32 +72,6 @@ const buildUpdateVariables = (resource, aorFetchType, params, queryType) =>
         if (params.data[key] === params.previousData[key]) {
             return acc;
         }
-        // if (Array.isArray(params.data[key])) {
-
-        //     // find mutation arg with propertyIds?
-        //     // TODO NOTE This isnt how hasura does it
-        //     const arg = queryType.args.find(a => a.name === `${key}Ids`);
-
-        //     if (arg) {
-        //         return {
-        //             ...acc,
-        //             [`${key}Ids`]: params.data[key].map(({ id }) => id),
-        //         };
-        //     }
-        // }
-
-        // // same as above but for objects
-        // // TODO NOTE This isnt how hasura does it
-        // if (typeof params.data[key] === 'object') {
-        //     const arg = queryType.args.find(a => a.name === `${key}Id`);
-
-        //     if (arg) {
-        //         return {
-        //             ...acc,
-        //             [`${key}Id`]: params.data[key].id,
-        //         };
-        //     }
-        // }
 
         return {
             ...acc,
@@ -168,33 +81,6 @@ const buildUpdateVariables = (resource, aorFetchType, params, queryType) =>
 
 const buildCreateVariables = (resource, aorFetchType, params, queryType) => {
     return Object.keys(params.data).reduce((acc, key) => {
-        // if (Array.isArray(params.data[key])) {
-        //     const arg = queryType.args.find(a => a.name === `${key}Ids`);
-        //     // if the field value is an array
-        //     // find an argument that takes that array whose name is "propertyids"
-        //     // ie if ur updating a user who has posts, they would have a postIDS property
-        //     // NOTE in hasura, it would not be called ..ids, it would be "todos_arr_rel_insert_input" etc
-        //     // TODO check how hasura handles updating references
-
-        //     if (arg) {
-        //         return {
-        //             ...acc,
-        //             [`${key}Ids`]: params.data[key].map(({ id }) => id),
-        //         };
-        //     }
-        // }
-
-        // if (typeof params.data[key] === 'object') {
-        //     const arg = true
-
-        //     if (arg) {
-        //         return {
-        //             ...acc,
-        //             [`${key}_id`]: params.data[key].id,
-        //         };
-        //     }
-        // }
-
         return {
             ...acc,
             [key]: params.data[key],
